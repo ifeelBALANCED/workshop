89928a94466d32ce29a0b1592170148ffa0ae577
balanced <vladislavherasymenko@gmail.com>
2023-03-10 13:33:48 +0200

    refactor(docker): configs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { SwaggerModule } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
 import { AppModule } from './app/app.module';
-import { config, corsOrigin, swaggerOptions } from './common/configs';
+import { corsOrigin } from './common/configs';
 import { get } from 'env-var';
 
 async function bootstrap() {
@@ -29,9 +28,9 @@ async function bootstrap() {
 
   const port = get('SERVER_PORT').asInt();
   const host = get('HOST').asString();
-  const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('api', app, document, swaggerOptions);
+  // const document = SwaggerModule.createDocument(app, config);
+  //
+  // SwaggerModule.setup('api', app, document, swaggerOptions);
 
   await app.listen(port, host);
 }
