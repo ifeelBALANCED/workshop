fd8fb987cfb1d4cc2cc7891dae234f00e1e5f35d
balanced <vladislavherasymenko@gmail.com>
2023-03-11 16:29:30 +0200

    test(e2e): article

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import * as request from 'supertest';
 import { useContainer } from 'class-validator';
 import { PrismaService } from 'nestjs-prisma';
 import { AppModule } from '../src/app/app.module';
+import { articlesData } from './mocks/article';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,23 +17,6 @@ describe('AppController (e2e)', () => {
     createdAt: expect.any(String),
     updatedAt: expect.any(String),
   });
-
-  const articlesData = [
-    {
-      id: 100001,
-      title: 'title1',
-      description: 'description1',
-      body: 'body1',
-      published: true,
-    },
-    {
-      id: 100002,
-      title: 'title2',
-      description: 'description2',
-      body: 'body2',
-      published: false,
-    },
-  ];
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -55,10 +39,115 @@ describe('AppController (e2e)', () => {
     });
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('GET /articles', () => {
+    it('returns a list of published articles', async () => {
+      const { status, body } = await request(app.getHttpServer()).get(
+        '/articles'
+      );
+
+      expect(status).toBe(200);
+      expect(body).toStrictEqual(expect.arrayContaining([articleShape]));
+      expect(body).toHaveLength(1);
+      expect(body[0].id).toEqual(articlesData[0].id);
+      expect(body[0].published).toBeTruthy();
+    });
+
+    it('returns a list of draft articles', async () => {
+      const { status, body } = await request(app.getHttpServer()).get(
+        '/articles/drafts'
+      );
+
+      expect(status).toBe(200);
+      expect(body).toStrictEqual(expect.arrayContaining([articleShape]));
+      expect(body).toHaveLength(1);
+      expect(body[0].id).toEqual(articlesData[1].id);
+      expect(body[0].published).toBeFalsy();
+    });
+  });
+
+  describe('GET /articles/{id}', () => {
+    it('returns a given article', async () => {
+      const { status, body } = await request(app.getHttpServer()).get(
+        `/articles/${articlesData[0].id}`
+      );
+
+      expect(status).toBe(200);
+      expect(body).toStrictEqual(articleShape);
+      expect(body.id).toEqual(articlesData[0].id);
+    });
+
+    it('fails to return non existing user', async () => {
+      const { status } = await request(app.getHttpServer()).get(
+        `/articles/100`
+      );
+
+      expect(status).toBe(404);
+    });
+
+    it('fails to return article with invalid id type', async () => {
+      const { status } = await request(app.getHttpServer()).get(
+        `/articles/string-id`
+      );
+
+      expect(status).toBe(400);
+    });
+  });
+
+  describe('POST /articles', () => {
+    it('creates an article', async () => {
+      const beforeCount = await prisma.article.count();
+
+      const { status } = await request(app.getHttpServer())
+        .post('/articles')
+        .send({
+          title: 'title3',
+          description: 'description3',
+          body: 'body3a',
+          published: false,
+        });
+
+      const afterCount = await prisma.article.count();
+
+      expect(status).toBe(201);
+      expect(afterCount - beforeCount).toBe(1);
+    });
+
+    it('fails to create article without title', async () => {
+      const { status } = await request(app.getHttpServer())
+        .post('/articles')
+        .send({
+          description: 'description4',
+          body: 'body4',
+          published: true,
+        });
+
+      expect(status).toBe(400);
+    });
+  });
+
+  describe('DELETE /articles/{id}', () => {
+    it('deletes an article', async () => {
+      const beforeCount = await prisma.article.count();
+
+      const { status } = await request(app.getHttpServer()).delete(
+        `/articles/${articlesData[0].id}`
+      );
+
+      const afterCount = await prisma.article.count();
+      expect(status).toBe(200);
+      expect(afterCount - beforeCount).toBe(-1);
+    });
+  });
+
+  describe('PUT /articles/{id}', () => {
+    it('fails to update article with invalid id type', async () => {
+      const { status } = await request(app.getHttpServer())
+        .put(`/articles/string-id`)
+        .send({
+          title: 'updated title',
+        });
+
+      expect(status).toBe(404);
+    });
   });
 });
---
+++ test/mocks/article.ts
@@ -0,0 +1,16 @@
+export const articlesData = [
+  {
+    id: 100001,
+    title: 'title1',
+    description: 'description1',
+    body: 'body1',
+    published: true,
+  },
+  {
+    id: 100002,
+    title: 'title2',
+    description: 'description2',
+    body: 'body2',
+    published: false,
+  },
+];
